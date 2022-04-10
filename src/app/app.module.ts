97332062acce20f1cd86cd32294c253efbedfaac
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2022-04-10 12:36:47 +0300

    login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,12 @@ import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     HttpClientModule,
   ],
