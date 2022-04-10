0570c616e8b1e1ee5d45463ca593ba2f8c5176fb
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2022-04-10 13:17:31 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/friends-messenger'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/friends-messenger/index.html'));
 });
